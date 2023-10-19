f05ae433c606ac06283829776ec438d3f9f6908b
Peter Kelvin Torver <techfortified@gmail.com>
2023-10-19 19:49:04 +0100

    fix error

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,5 @@
 export const getData =  async () =>{
-    const res = await fetch('http://localhost:3000/api')
+    const res = await fetch('/api')
     if (!res.ok) {
       // This will activate the closest `error.js` Error Boundary
       throw new Error('Failed to fetch data')
